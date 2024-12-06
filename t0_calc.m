0ccfb2ce26afc411be496bb1a8ba75c2ed56fc78
TheeBrainiacs <145912020+Kronbii@users.noreply.github.com>
2024-12-06 21:24:56 +0200

    plots

--- t0_calc.m
+++ t0_calc.m
@@ -66,3 +66,87 @@ legend show;
 title('Torque Plot');
 grid on;
 disp('doneee')
+
+% Plot coupler_angle0
+figure;
+plot(crank_angle, coupler_angle0);
+title('Coupler Angle0');
+xlabel('Crank Angle (degrees)');
+ylabel('Coupler Angle0');
+
+% Plot a4y
+figure;
+plot(crank_angle, a4y);
+title('a4y');
+xlabel('Crank Angle (degrees)');
+ylabel('a4y (m/s^2)');
+
+% Plot Fg
+figure;
+plot(crank_angle, Fg);
+title('Fg');
+xlabel('Crank Angle (degrees)');
+ylabel('Fg (N)');
+
+% Plot alpha3
+figure;
+plot(crank_angle, alpha3);
+title('Alpha3');
+xlabel('Crank Angle (degrees)');
+ylabel('Alpha3 (rad/s^2)');
+
+% Plot a3x
+figure;
+plot(crank_angle, a3x);
+title('a3x');
+xlabel('Crank Angle (degrees)');
+ylabel('a3x (m/s^2)');
+
+% Plot a3y
+figure;
+plot(crank_angle, a3y);
+title('a3y');
+xlabel('Crank Angle (degrees)');
+ylabel('a3y (m/s^2)');
+
+% Plot m3 (constant value)
+figure;
+plot(crank_angle, m3 * ones(size(crank_angle)));
+title('m3 (Mass of Link 3)');
+xlabel('Crank Angle (degrees)');
+ylabel('m3 (kg)');
+
+% Plot m2 (constant value)
+figure;
+plot(crank_angle, m2 * ones(size(crank_angle)));
+title('m2 (Mass of Link 2)');
+xlabel('Crank Angle (degrees)');
+ylabel('m2 (kg)');
+
+% Plot m4 (constant value)
+figure;
+plot(crank_angle, m4 * ones(size(crank_angle)));
+title('m4 (Mass of Link 4)');
+xlabel('Crank Angle (degrees)');
+ylabel('m4 (kg)');
+
+% Plot J3 (constant value)
+figure;
+plot(crank_angle, J3 * ones(size(crank_angle)));
+title('J3 (Moment of Inertia of Link 3)');
+xlabel('Crank Angle (degrees)');
+ylabel('J3 (kg*m^2)');
+
+% Plot gravitational constant g (constant value)
+figure;
+plot(crank_angle, g * ones(size(crank_angle)));
+title('Gravitational Constant (g)');
+xlabel('Crank Angle (degrees)');
+ylabel('g (m/s^2)');
+
+% Plot crank_length (constant value)
+figure;
+plot(crank_angle, crank_length * ones(size(crank_angle)));
+title('Crank Length');
+xlabel('Crank Angle (degrees)');
+ylabel('Crank Length (m)');
